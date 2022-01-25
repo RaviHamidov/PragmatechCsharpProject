5c58e13ee9643ad9ba2cc2f8ae8d688269d371d4
RaviHamidov <ravihamidov42@gmail.com>
2022-01-25 18:56:16 +0400

    NonGenericCollectionsPractise

---
+++ 09_Lesson/01_Task/TaskOne/TaskOne/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace TaskOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // non generic collection\
+
+            // Fifo
+            ArrayList arraylist = new ArrayList();
+            arraylist.Add(5);
+            arraylist.Add(6);
+            arraylist.Add(7);
+            arraylist.Add("ajkhflef");
+            arraylist.Add(true);
+            foreach (var item in arraylist)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(" - - - - - - - - - ");
+
+            // Lifo
+            Stack stack = new Stack();
+            stack.Push(2);
+            stack.Push("efjkw");
+            stack.Push(true);
+            stack.Push("push");
+            
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(stack.Pop() + " remove");
+
+            Console.WriteLine(" - - - - - - - - - ");
+
+            //Fifo
+            Queue queue = new Queue();
+            queue.Enqueue(2);
+            queue.Enqueue(1);
+            queue.Enqueue(4);
+            queue.Enqueue(6);
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);       
+            }
+
+            Console.WriteLine(queue.Dequeue());
+
+
+
+        }
+    }
+}
